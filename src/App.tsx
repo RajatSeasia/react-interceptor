8dbdd97a3783085104ab2047a6a6d8044b4d98a6
seasia-amar <singhamarjeet1@seasiainfotech.com>
2024-01-19 10:32:12 +0530

    new react interceptor

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-3xl text-red-700 font-bold underline">
+      Hello world! new check
+    </h1>
+
+    </>
+  )
+}
+
+export default App
